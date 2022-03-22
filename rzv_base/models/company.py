a84b174720348ef2f668db4d4656e3598982a62e
ranoa <tsiky@alphamada.com>
2022-03-22 15:26:01 +0300

    FEAT[rzv_base]: add nif stat cif on company info

--- rzv_base/models/company.py
+++ rzv_base/models/company.py
@@ -2,6 +2,7 @@ from odoo import models, fields, api
 class Company(models.Model):
     _inherit="res.company"
 
-    cif = fields.Char("CIF", company_dependent=True)
+
     nif=fields.Char("NIF",company_dependent=True)
+    cif = fields.Char("CIF", company_dependent=True)
     stat=fields.Char("STAT",company_dependent=True)
